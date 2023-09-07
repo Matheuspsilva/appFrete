b2c8c8c62d7eae75deea2e8c405920350136cfe4
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-09-07 16:56:39 -0300

    Commit inicial

---
+++ src/main/java/com/matheussilvadev/appfrete/AppfreteApplication.java
@@ -0,0 +1,13 @@
+package com.matheussilvadev.appfrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheussilvadev/appfrete/AppfreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheussilvadev.appfrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
