60f787ed769bbc1d171b1b00e16f6316593f8cd7
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-09-28 20:26:03 -0300

    Compartilhar uma listagem de destinatarios com a camada de visão

--- src/main/java/com/matheussilvadev/appfrete/domain/controller/DestinatarioController.java
+++ src/main/java/com/matheussilvadev/appfrete/domain/controller/DestinatarioController.java
@@ -1,13 +1,26 @@
 package com.matheussilvadev.appfrete.domain.controller;
 
+import com.matheussilvadev.appfrete.domain.model.Destinatario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.AttributedString;
+import java.util.List;
 
 @Controller
 public class DestinatarioController {
 
         @GetMapping("/destinatario/lista")
-        public String telaLista() {
+        //Criar um model nos parametros do metodo
+        public String telaLista( Model model) {
+            Destinatario destinatario1 = new Destinatario( 1L, "Destinatario 1", "Rua 1", "999999991", "11111111111111" );
+            Destinatario destinatario2 = new Destinatario( 2L, "Destinatario 2", "Rua 2", "999999992", "22222222222222" );
+            Destinatario destinatario3 = new Destinatario( 3L, "Destinatario 3", "Rua 3", "999999993", "33333333333333" );
+
+            List<Destinatario> listaDestinatario = List.of(destinatario1, destinatario2, destinatario3);
+            model.addAttribute("listaDestinatario", listaDestinatario);
+
             return "destinatario/lista";
         }
 }
--- src/main/java/com/matheussilvadev/appfrete/domain/model/Empresa.java
+++ src/main/java/com/matheussilvadev/appfrete/domain/model/Empresa.java
@@ -26,4 +26,43 @@ public abstract class Empresa {
 
      public abstract String impressao();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
