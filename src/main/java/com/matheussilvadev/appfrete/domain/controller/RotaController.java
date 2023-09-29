8ea31d2475b4881802c03af4340fc8115448402e
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-09-28 21:10:16 -0300

    Compartilhar uma listagem de rotas com a camada de visão

--- src/main/java/com/matheussilvadev/appfrete/domain/controller/RotaController.java
+++ src/main/java/com/matheussilvadev/appfrete/domain/controller/RotaController.java
@@ -1,13 +1,26 @@
 package com.matheussilvadev.appfrete.domain.controller;
 
+import com.matheussilvadev.appfrete.domain.model.Rota;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class RotaController {
 
     @GetMapping("/rota/lista")
-    public String telaLista() {
+    public String telaLista(Model model) {
+
+        Rota rota1 = new Rota(100.5f);
+        Rota rota2 = new Rota(200.3f);
+        Rota rota3 = new Rota(300.1f);
+
+        List<Rota> rotas = List.of(rota1, rota2, rota3);
+
+        model.addAttribute("listaRotas", rotas);
+
         return "/rota/lista";
     }
 }
